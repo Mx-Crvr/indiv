2f4aaddd222964d6a3fa4779a14adefe58ce2778
Mx-Crvr <maxevans.dev@gmail.com>
2023-11-09 15:10:09 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -15,31 +15,35 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.post('/individual', (req, res) => {
+if (process.env.NODE_ENV === 'production') {
+  app.post('/individual', (req, res) => {
 
-  base('Thrive Brochures').create([
-    {
-      "fields": {
-        "Email address": req.body.email,
-        "Brochure Type": "Individual"
+    base('Thrive Brochures').create([
+      {
+        "fields": {
+          "Email address": req.body.email,
+          "Brochure Type": "Individual"
+        }
+      },
+    ], function(err, records) {
+      if (err) {
+        console.error(err);
+        return;
       }
-    },
-  ], function(err, records) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    records.forEach(function (record) {
-      console.log(record.getId());
+      records.forEach(function (record) {
+        console.log(record.getId());
+      });
     });
-  });
+  
+  
+    // Redirect to pdf
+    res.statusCode = 301;
+    res.setHeader('Location', 'https://assets-global.website-files.com/5f048698055f743dd87fba6e/654c82936d4563a7de62f1e2_Thrive%20Brochure_Individual.pdf');
+    res.end();
+  })
+}
 
 
-  // Redirect to pdf
-  res.statusCode = 301;
-  res.setHeader('Location', 'https://assets-global.website-files.com/5f048698055f743dd87fba6e/654c82936d4563a7de62f1e2_Thrive%20Brochure_Individual.pdf');
-  res.end();
-})
 
 app.listen((process.env.PORT), () => {
   console.log(`http://localhost:${process.env.PORT}`);
